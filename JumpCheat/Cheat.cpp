4bd5b494c7e404b3110266187c7dddfee1efb79a
新瑞 杨 <www.yangxinruei@qq.com>
2018-01-06 18:06:50 +0800

    调节关键参数 精确跳跃距离

--- JumpCheat/Cheat.cpp
+++ JumpCheat/Cheat.cpp
@@ -70,7 +70,7 @@ bool Cheat::nativeEventFilter(const QByteArray & eventType, void * message, long
 	else if (parameter == hk3) {
 		double t = sqrt((end.x - start.x)*(end.x - start.x) + (end.y - start.y)*(end.y - start.y));
 
-		t *= 2.8;
+		t *= 2.7;
 
 		int s = (int)t;
 
